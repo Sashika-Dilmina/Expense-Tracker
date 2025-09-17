d769b94c9a7051698135b5ada35e6ca1edae06e2
Inuka Mapa <139098767+InukaMapa@users.noreply.github.com>
2025-09-17 22:53:41 +0530

    l

--- backend/server.js
+++ backend/server.js
@@ -44,4 +44,3 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-lmlmpm
