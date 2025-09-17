96f88d8d6ca6f25a05cbd8ffe0d054a7589d3d14
Inuka Mapa <139098767+InukaMapa@users.noreply.github.com>
2025-09-17 22:52:35 +0530

    demo

--- backend/server.js
+++ backend/server.js
@@ -44,3 +44,4 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+lmlmpm
--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -3,4 +3,4 @@ import React from "react";
 const Home = () => {
   return <div>Home</div>;
 };
-export default Home;
+export default Home; 
